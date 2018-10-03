3055996e67aa8d610f1ac5dc350efd3865c46faa
v-turin <vladimir.turin@opencascade.com>
2018-10-03 19:07:00 +0300

    Linux build

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# flann-feedstock
+About flann
+===========
+
+Home: http://www.cs.ubc.ca/research/flann/
+
+Package license: BSD 3-Clause
+
+Summary: The Fast Library for Approximate Nearest Neighbors
---
+++ recipe/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# cannot build flann from within the source directory
+mkdir build
+cd build
+
+CUDA="OFF"
+if [ "$cuda_impl" == "cuda" ]; then
+    CUDA="ON"
+    # build with c++11
+    export CXXFLAGS=$(echo $CXXFLAGS | sed "s/-std=c++17/-std=c++11/")
+fi
+
+cmake -G "Ninja" \
+	 -DCMAKE_INSTALL_PREFIX=$PREFIX \
+	 -DBUILD_MATLAB_BINDINGS:BOOL=OFF \
+	 -DBUILD_PYTHON_BINDINGS:BOOL=ON \
+	 -DBUILD_EXAMPLES:BOOL=OFF \
+	 -DBUILD_DOC:BOOL=OFF \
+	 -DBUILD_CUDA_LIB:BOOL=$CUDA \
+	 ..
+
+ninja install
